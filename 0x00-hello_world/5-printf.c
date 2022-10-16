22f5517773d930eee41e96c259a1653535498716
cloud9n <reubenindiya@gmail.com>
2022-10-16 07:41:32 -0700

    Updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /*
- * main - Entry point
+ * Main - Entry point
  * Return: Alwyas 0 (Success)
  */
 
-int main(void) 
+int main(void) /* Main -Entry point */
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
