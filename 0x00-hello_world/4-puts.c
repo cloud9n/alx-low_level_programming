0fe425da09b456c3ea9deb47c07559155a9c481e
cloud9n <reubenindiya@gmail.com>
2022-10-16 12:42:57 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  *Return: Always 0 (Success)
  */
 int main(void) 
-	 /*  Main Entry - main() */
+	 /*Main Entry - main()*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0); /*Return: Always 0 (Success)*/
