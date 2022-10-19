546b018bf6605a27c2d4a03c53efb3b928ec13a1
cloud9n <reubenindiya@gmail.com>
2022-10-19 09:08:47 -0700

    Task 7 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes
+ * @n: the nth in the question
+ * Return: he absolute value of the int
+ */
+int_abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
