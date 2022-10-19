41a1a42a2d4c80d60b92bc0014869eaea08a0db3
cloud9n <reubenindiya@gmail.com>
2022-10-18 22:03:20 -0700

    task 2 completed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check c if its lower case
+ * author: indiya Reuben
+ * Return: 1 if statement is true and 0 if not
+ * _islower
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+		return (l);
+
+	else return(0);
+}
+
