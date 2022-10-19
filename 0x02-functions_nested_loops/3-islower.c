8299925c19d61883114a874f617d88011f375458
cloud9n <reubenindiya@gmail.com>
2022-10-18 22:16:56 -0700

    Task 3 corrected

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,11 @@
 /**
  * _islower - check c if its lower case
  * author: indiya Reuben
+ * c
+ * C - is what wee will be checking
  * Return: 1 if statement is true and 0 if not
  * _islower
+ * C: is what we are checking
  */
 
 int _islower(int c)
@@ -12,7 +15,8 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
+	else
+		return (0);
 
-	else return(0);
 }
 
