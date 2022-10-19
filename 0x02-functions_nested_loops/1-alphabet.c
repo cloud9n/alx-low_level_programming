356dfcb2322104c5a1c4fdc53b4d1a1af467168f
cloud9n <reubenindiya@gmail.com>
2022-10-18 20:56:10 -0700

    Task 1 corrected

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet - a - z in lower case
  * author: indiya Reuben
- * Return: is 0
+ * 
  */
 
 void print_alphabet(void)
@@ -13,6 +13,6 @@ void print_alphabet(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	_putchar("\n");
+	_putchar('\n');
 }
 
