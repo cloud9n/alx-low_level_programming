4090c2cb2fb2a0f5790583a2e2a733eb2a0c201a
cloud9n <reubenindiya@gmail.com>
2022-10-19 04:57:01 -0700

    Task 5 done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/**
+ * print_sign -print a sign of numbers
+ * Return: the return is 1 or 0, -1 and n
+ * @n: this is the number to be checked
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putcher('+');
+		return (0);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+
+	}
+}
