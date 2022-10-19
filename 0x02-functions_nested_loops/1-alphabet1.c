dde21c6cb6f1d251702c2697521c1a5c215ef9d3
cloud9n <reubenindiya@gmail.com>
2022-10-18 20:46:31 -0700

    Task 1 corrected

---
+++ 0x02-functions_nested_loops/1-alphabet1.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a - z in lower case
+ * author: indiya Reuben
+ * Return: is 0
+ */
+
+void print_alphabet(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar("\n");
+}
+
